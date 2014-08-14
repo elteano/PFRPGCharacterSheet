569a9228752ad877f1ee04baae6adf4ab43d9cfa
Thomas Tucker <elteano@gmail.com>
2014-08-14 12:25:08 -0700

    Commented.

--- CharacterSheet/src/org/elteano/charactersheet/model/PlayerClass.java
+++ CharacterSheet/src/org/elteano/charactersheet/model/PlayerClass.java
@@ -10,9 +10,18 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.util.Log;
 
+/**
+ * Class for storing information about a player's classes.
+ */
 public class PlayerClass implements Parcelable, Serializable {
 
+	/**
+	 * Serializable stuff.
+	 */
 	public static final long serialVersionUID = 1L;
+	/**
+	 * Parcelable stuff.
+	 */
 	public static Parcelable.Creator<PlayerClass> CREATOR = new Parcelable.Creator<PlayerClass>() {
 
 		public PlayerClass createFromParcel(Parcel source) {
@@ -24,6 +33,16 @@ public class PlayerClass implements Parcelable, Serializable {
 		}
 	};
 
+	/**
+	 * Turns a list of player classes into a String as might be seen on a
+	 * character sheet.
+	 *
+	 * Example: a barbarian 5 and bard 3 would end up as barbarian 5 / bard 3
+	 *
+	 * @param classes
+	 *            The list of classes to stringify.
+	 * @return A presentable string.
+	 */
 	public static String compoundToString(ArrayList<PlayerClass> classes) {
 		String ret = "";
 		for (PlayerClass c : classes) {
@@ -34,6 +53,14 @@ public class PlayerClass implements Parcelable, Serializable {
 		return ret;
 	}
 
+	/**
+	 * Interprets a textual list of classes and turns it into a list of objects
+	 * for internal use.
+	 *
+	 * @param listString
+	 *            A String list of classes delimited by forward slashes.
+	 * @return An object list of classes.
+	 */
 	public static ArrayList<PlayerClass> interpretListString(String listString) {
 		ArrayList<PlayerClass> ret = new ArrayList<PlayerClass>();
 		for (String classLevel : listString.split("/")) {
@@ -52,23 +79,50 @@ public class PlayerClass implements Parcelable, Serializable {
 		return ret;
 	}
 
+	/**
+	 * The number of levels invested in this class.
+	 */
 	private int levels;
 
+	/**
+	 * The name of the class.
+	 */
 	private String name;
 
+	/**
+	 * Creates a class with the given name and level.
+	 *
+	 * @param name
+	 *            The class's name.
+	 * @param levels
+	 *            The number of character levels invested into the class.
+	 */
 	public PlayerClass(String name, int levels) {
 		this.name = name;
 		this.levels = levels;
 	}
 
+	/**
+	 * Parcelable stuff.
+	 */
 	public int describeContents() {
 		return 0;
 	}
 
+	/**
+	 * Gets the number of character levels invested into the class.
+	 *
+	 * @return The number of character levels invested into the class.
+	 */
 	public int getLevels() {
 		return levels;
 	}
 
+	/**
+	 * Gets the name of the class.
+	 *
+	 * @return The name of the class.
+	 */
 	public String getName() {
 		return name;
 	}
